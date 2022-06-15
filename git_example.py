c318de6b7e0082a5d441b095c6c4b2836a674dd4
JulianGuerreroH <julian.guerrero@mail.escuelaing.edu.co>
2022-06-15 00:11:15 -0500

    asdad

---
+++ git_example.py
@@ -0,0 +1,8 @@
+#git clone-b main https://github.com/JulianGuerreroH/mintic-C2.git
+#es la URL que me da la pagina en github
+#main es el nombre de la rama 
+
+print("Hola mundo")
+print("Primer cambio")
+print("Segundo cambio")
+#comentario 1
